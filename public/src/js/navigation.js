6177d9c0eb996e6c3b9d5275815b032a7b2f7351
Qing Huang <hq1992518@msn.cn>
2015-11-13 14:21:22 -0800

    navigation,notification

---
+++ public/src/js/navigation.js
@@ -0,0 +1,41 @@
+$( document ).ready(function() {
+  Parse.initialize("ULppY5RxxZUo8yekihZdVH3uHLm24j5Q6298Un4O",
+                   "mDAyhkdhlv6qH9lT9WFzMCeML6ycMa1S8oWlybVG");	  	
+
+			   
+});
+
+
+function Logout() {
+
+  // Let's check if the browser supports notifications
+  if (!("Notification" in window)) {
+    alert("This browser does not support desktop notification");
+  }
+
+  // Let's check whether notification permissions have already been granted
+  else if (Notification.permission === "granted") {
+    // If it's okay let's create a notification
+    var notification = new Notification(
+				"Log Out Securely", {
+					body: "Bye-bye."
+				}
+			);
+  }
+
+  // Otherwise, we need to ask the user for permission
+  else if (Notification.permission !== 'denied') {
+    Notification.requestPermission(function (permission) {
+      // If the user accepts, let's create a notification
+      if (permission === "granted") {
+        var notification = new Notification("Hi there!");
+      }
+    });
+	console.log(Notification.permission);
+  }
+  // At last, if the user has denied notifications, and you 
+  // want to be respectful there is no need to bother them any more.
+	
+  Parse.User.logOut();
+
+}
