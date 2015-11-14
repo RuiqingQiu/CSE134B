dbd8536862d19763f5a8be7013cb115b9fd1f696
Ruiqing Qiu <raymond.qiu1993@gmail.com>
2015-11-14 15:56:33 -0800

    list and edit communication done

--- public/src/js/edit.js
+++ public/src/js/edit.js
@@ -1,5 +1,25 @@
-var GLOBAL = {};
-GLOBAL.objectID = "ZAxIn2CEPh";
+var QueryString = function () {
+  // This function is anonymous, is executed immediately and 
+  // the return value is assigned to QueryString!
+  var query_string = {};
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i=0;i<vars.length;i++) {
+    var pair = vars[i].split("=");
+        // If first entry with this name
+    if (typeof query_string[pair[0]] === "undefined") {
+      query_string[pair[0]] = decodeURIComponent(pair[1]);
+        // If second entry with this name
+    } else if (typeof query_string[pair[0]] === "string") {
+      var arr = [ query_string[pair[0]],decodeURIComponent(pair[1]) ];
+      query_string[pair[0]] = arr;
+        // If third or later entry with this name
+    } else {
+      query_string[pair[0]].push(decodeURIComponent(pair[1]));
+    }
+  } 
+    return query_string;
+}();
 
 $(document).ready(function() {
 	Parse.initialize("ULppY5RxxZUo8yekihZdVH3uHLm24j5Q6298Un4O",
@@ -9,7 +29,7 @@ $(document).ready(function() {
     if(Parse.User.current() == null || Parse.User.current().authenticated() == null) {
         window.location.href = "login.html";
     }
-    getHabit()
+    getHabit();
 });
 
 function getHabit(){
@@ -17,12 +37,14 @@ function getHabit(){
     var habit = Parse.Object.extend("Habit");
     // var currUser = Parse.User.current();
     var query = new Parse.Query(habit);
-    query.equalTo("objectId", GLOBAL.objectID);
+    query.equalTo("objectId", QueryString.objectID);
     query.find({
         success: function(objs) {
         	var current_object = objs[0];
         	document.forms["habitForm"]["fullname"].value = current_object.get('title');
         	document.getElementById("icon4").src = current_object.get('icon_image')._url;
+        	document.getElementById("cropped").src = current_object.get('icon_image')._url;
+
         	selectImage('icon4');
         	var weekly_freq = current_object.get('weekly_frequency');
         	console.log(weekly_freq);
@@ -44,6 +66,32 @@ function getHabit(){
             alert("Error: " + error.code + " " + error.message);
         }                
     });
+}
 
-    
+
+function saveEditedHabit(el){            
+    var habit = Parse.Object.extend("Habit");
+    var query = new Parse.Query(habit);
+    var toDelete = query.get($(el).attr("id"), {
+        success: function(obj) {
+            var daily_current = obj.get("daily_current");
+            if(daily_current < obj.get("daily_frequency")){
+                obj.set("daily_current", ++daily_current);
+                obj.save({
+                    success: function(obj) {
+                        var msg = $(el).find(".message-today");
+                        // alert(daily_current);
+                        msg.children(".daily-current").text(daily_current);
+                        msg.css("visibility","visible");
+                    },
+                    error: function(obj, error) {
+                        alert("Error: " + error.code + " " + error.message);
+                    }
+                });
+            }
+        },
+        error: function(obj, error) {
+            alert("Error: " + error.code + " " + error.message);
+        }
+    });
 }
