1ec894007f0bf58d3c9e9a241760bdb365eed350
Timothy Chang <timothywchang@yahoo.com>
2015-11-19 11:02:47 -0800

    Fixed code formatting

--- public/src/js/login.js
+++ public/src/js/login.js
@@ -1,63 +1,64 @@
-$( document ).ready(function() {
+$(document).ready(function() {
   Parse.initialize("ULppY5RxxZUo8yekihZdVH3uHLm24j5Q6298Un4O",
-                   "mDAyhkdhlv6qH9lT9WFzMCeML6ycMa1S8oWlybVG");
-  if(Parse.User.current() != null && Parse.User.current().authenticated()) {
+    "mDAyhkdhlv6qH9lT9WFzMCeML6ycMa1S8oWlybVG");
+  if (Parse.User.current() !== null && Parse.User.current().authenticated()) {
     window.location.href = "welcome.html";
   }
 });
 
 function logIn(e) {
-      var username = $("#usermail").val();
-      var password = $("#password").val();
+  var username = $("#usermail").val();
+  var password = $("#password").val();
 
-      Parse.User.logIn(username, password, {
-        success: function(user) {
-          console.log("Successfully logged in!");
-          window.location.href = "welcome.html";
-        },
+  Parse.User.logIn(username, password, {
+    success: function(user) {
+      console.log("Successfully logged in!");
+      window.location.href = "welcome.html";
+    },
 
-        error: function(user, error) {
-          console.log("error!");
-          errorMessage = error.message;
-          switch(error.code) {
-            case 101:
-              errorMessage = "Invalid username or password."
-              break;
-            case 100:
-              errorMessage = "Internet is disconnected."
-              break;
-          }
-          showMessage(errorMessage);
-        }
-      });
-
-      return false;
+    error: function(user, error) {
+      console.log("error!");
+      errorMessage = error.message;
+      switch (error.code) {
+        case 101:
+          errorMessage = "Invalid username or password."
+          break;
+        case 100:
+          errorMessage = "Internet is disconnected."
+          break;
+      }
+      showMessage(errorMessage);
+    }
+  });
+  return false;
 }
 
 function signUp(e) {
-      var username = $("#usermail").val();
-      var password = $("#password").val();
+  var username = $("#usermail").val();
+  var password = $("#password").val();
 
-      Parse.User.signUp(username, password, { ACL: new Parse.ACL() }, {
-        success: function(user) {
-          console.log("Successfully logged in!");
-          window.location.href = "welcome.html";
-        },
+  Parse.User.signUp(username, password, {
+    ACL: new Parse.ACL()
+  }, {
+    success: function(user) {
+      console.log("Successfully logged in!");
+      window.location.href = "welcome.html";
+    },
 
-        error: function(user, error) {
-          console.log("error!");
-          errorMessage = error.message;
-          switch(error.code) {
-            case 202:
-              errorMessage = "Username is already taken."
-              break;
-            case 100:
-              errorMessage = "Internet is disconnected."
-              break;
-          }
-          showMessage(errorMessage);
-        }
-      });
+    error: function(user, error) {
+      console.log("error!");
+      errorMessage = error.message;
+      switch (error.code) {
+        case 202:
+          errorMessage = "Username is already taken."
+          break;
+        case 100:
+          errorMessage = "Internet is disconnected."
+          break;
+      }
+      showMessage(errorMessage);
+    }
+  });
 }
 
 function showMessage(str) {
@@ -73,5 +74,4 @@ function onClickSignUp() {
   signUpText.style.display = "block";
   var message = document.getElementById("message");
   message.style.display = "none";
-
 }
